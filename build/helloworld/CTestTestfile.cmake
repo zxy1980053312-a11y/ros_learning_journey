8b3d4e7e615d4b2f301e9d27d8b43b85348549e5
zxy1980053312-a11y <zxy1980053312@gmail.com>
2026-04-18 23:55:34 +0800

    Clean up build files and fix symlink

--- build/helloworld/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zxy/study_ws/src/helloworld
-# Build directory: /home/zxy/study_ws/build/helloworld
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
